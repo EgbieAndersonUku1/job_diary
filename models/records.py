4a3a35c51d571f7afd4cefc16b576ca218905b56
EgbieAndersonUku1 <jayunderwood2011@hotmail.com>
2016-08-21 00:59:45 +0100

    Update records.py

--- models/records.py
+++ models/records.py
@@ -9,9 +9,10 @@
 ####################################################################################
 
 from database import DataBase as db
-from translator import translate_to_month_num, get_hours_worked, gen_row_id
+from translator import translate_to_month_num, gen_row_id, get_hours_worked
 import random
 import time
+import uuid
 
 class Records(object):
     """Records (class)
@@ -19,24 +20,27 @@ class Records(object):
     to either delete, retreive or update the record details of the user.
     """
     def __init__(self, job_title, descr, loc, start_time, finish_time,
-                 hourly_rate, user_id, daily_rate,
-                 date, day, row_id, _id, hours_worked=None, month=None):
+                           hourly_rate, total_hours, _hours, user_id, daily_rate,
+                          date, day, month, row_id=None, _id=None):
 
-        self.job_title = job_title
+        self.job_title   = job_title
         self.descr = descr
-        self.loc = loc
-        self.start_time = start_time
+        self.daily_rate  = daily_rate
+        self.start_time  = start_time
         self.finish_time = finish_time
         self.hourly_rate = hourly_rate
-        self.hours_worked = hours_worked
+        self.total_hours = total_hours # hours in words e.g 2 hrs and 10 mins
+        self._hours  = _hours                 # hours in float 2.10 e.g 2 hrs and .10 mins for db comparision
         self.user_id = user_id
-        self.daily_rate  = daily_rate
-        self.date = time.strftime("%d/%m/%Y") if date is None else date
-        self.day  = time.strftime('%A') if day is None else day
-        self.month = self.date.split('/')[1] # split the date by '/' and take the month part
-        self.row_id = row_id
-        self._id = _id
+        self.date  =  date
+        self.day   =  day
+        self.loc   = loc
+        self.row_id = gen_row_id() if row_id is None else row_id
+        self.month = month
+        self._id = uuid.uuid4().hex if _id is None else _id
         self.track_times  = {}
+
+
 
     @classmethod
     def _find(cls, query):
@@ -65,35 +69,35 @@ class Records(object):
         return cls._find_one(query={'row_id':row_id})
 
     @classmethod
-    def find_by_date(cls, date):
-        """Retreives the job by using the date"""
-        return cls._find(query={'date': date})
-
-    @classmethod
-    def find_by_date_and_day(cls, date, day):
-        """Retreives the job using the date and day"""
-        return cls._find({'date':date, 'day':day.title()})
-
+    def find_by_date_or_day(cls, date, day):
+        """Retreives the job using the date or day"""
+        if date and day:
+            return cls._find({'date':date, 'day':day.title()})
+        elif date and not day:
+            return cls._find(query={'date': date})
+        elif day and not date:
+            return cls._find(query={'day': day})
+        
     @classmethod
     def find_by_month(cls, month, month2):
-	"""find_by_month(str, str) -> return(None or obj)
+        """find_by_month(str, str) -> return(None or obj)
 
-	The method find_by_month receives a strings e.g. January from the User inteface.
-	It then translates it into a month number since the months are stored in the database
-	as numbers. (This makes it easy to query two months e.g. find days worked between
-	the month January and Feb which includes the starting and ending month January
-	and February.) It then uses the number to query the database for those days.
-	"""
-	if month and not month2:
-	    return cls._find(query={'month':translate_to_month_num(month)}) # user wants information for a single month
-	elif month and month2: # user wants information between two given months
+        The method find_by_month receives a strings e.g. January from the User inteface.
+        It then translates it into a month number since the months are stored in the database
+        as numbers. (This makes it easy to query two months e.g. find days worked between
+        the month January and Feb which includes the starting and ending month January
+        and February.) It then uses the number to query the database for those days.
+        """
+        if month and not month2:
+            return cls._find(query={'month':translate_to_month_num(month)}) # user wants information for a single month
+        elif month and month2: # user wants information between two given months
 
-	    month  = translate_to_month_num(month)  # translate month to number
-	    month2 = translate_to_month_num(month2) # translate month2 to number
-	    month, month2 = min(month, month2), max(month, month2) # ensure that month1 is less then month2
+            month  = translate_to_month_num(month)  # translate month to number
+            month2 = translate_to_month_num(month2) # translate month2 to number
+            month, month2 = min(month, month2), max(month, month2) # ensure that month1 is less then month2
 
-	    # return days worked between the two months given including the starting and ending month
-	    return cls._find(query={'month': {'$gte': month, "$lte":month2}})
+            # return days worked between the two months given including the starting and ending month
+            return cls._find(query={'month': {'$gte': month, "$lte":month2}})
 
     @classmethod
     def find_by_location(cls, loc):
@@ -147,9 +151,9 @@ class Records(object):
 
     @staticmethod
     def delete_row(row_id):
-	    """deletes the row using the id"""
+        """deletes the row using the id"""
         return db.delete_row(collections='jobs_details', query={'row_id': '#'+str(row_id)})
-        
+
     def save(self):
         """saves the data to the databases. It is saved in the form of json"""
         db.insert('jobs_details', self.get_json())
@@ -157,18 +161,18 @@ class Records(object):
     def get_json(self):
         """returns a json represent of the class"""
 
-        return { 'job_title'   : self.job_title.title(),
-                 'descr'       : self.descr.title(),
-                 'loc'         : self.loc.title(),
-                 'start_time'  : self.start_time,
-                 'finish_time' : self.finish_time,
-                 'hours_worked': self.get_hours(),
-                 'hourly_rate' : self.hourly_rate,
-                 'daily_rate'  : self.daily_rate,
-		 'month'       : self.month,
-                 'date'        : time.strftime("%d/%m/%Y"),
-                 'day'         : time.strftime('%A'),
-		 'month'       : self.month,
-                 'user_id'     : self.user_id,
-                 'row_id'      : gen_row_id()
-                 }
+        return { 'job_title'  : self.job_title.title(),
+                 'descr'      : self.descr.title(),
+                 'loc'        : self.loc.title(),
+                 'start_time' : self.start_time,
+                 'finish_time': self.finish_time,
+                 'hourly_rate': self.hourly_rate,
+                 'total_hours': self.total_hours,
+                 '_hours'      : self._hours,
+                 'user_id'    : self.user_id,
+                 'daily_rate' : self.daily_rate,
+                 'date'       : self.date,
+                 'month'      : self.month,
+                 'row_id'     : self.row_id,
+                 'day'        : self.day,
+                 '_id'        : self._id}
