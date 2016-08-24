f4965eceb0c7f0a181e1d63c53ba90260425af13
jayunderwood2011 <jayunderwood2011@hotmail.com>
2016-08-24 01:44:03 +0100

    updating unit test files

--- src/unit_test/user_test.py
+++ src/unit_test/user_test.py
@@ -17,9 +17,38 @@ loc        = 'London'
 start_time = '10:00'
 finish_time = '21:00'
 hourly_rate = '150.00'
+total_hours =  '9'
+month       = '08'
+
 
 user  = User(name, start_date, end_date, day, id)
+def is_data_correct(obj):
+    count = 0
+    def is_true(value1, value2):
+        if value1 == value2.title():
+            global count
+            count += 1
 
+    if not obj:
+        return False
+
+    obj = obj[0]
+    print '[+] Testing job title parameter', is_true(obj.job_title, job_title.title())
+    print '[+] Testing job description parameter', is_true(obj.descr,job_title.title())
+    print '[+] Testing daily rate parameter', is_true(obj.daily_rate, daily_rate)
+    print '[+] Testing day parameter', is_true(obj.start_time, start_time)
+    print '[+] Testing job_title parameter', is_true(obj.finish_time, finish_time)
+    print '[+] Testing description parameter', is_true(obj.hourly_rate, hourly_rate)
+    print '[+] Testing location parameter', is_true(obj.total_hours, total_hours)
+    return count
+
+def does_obj_exists(func, arg):
+    rec_obj = func(arg)[0]
+    if rec_obj:
+        print 'Objection successful retreived data obj'
+        sleep(0.05)
+        print 'Checking whether the data matches the given parameters'
+        sleep(0.05)
 
 def run_test():
 
@@ -30,29 +59,43 @@ def run_test():
     print 'Running a series of test to ..'
 
     print '[+] Add a series of jobs details to the user object '
+    print """ [+] {}
+              [+] {}
+              [+] {}
+              [+] {}
+              [+] {}
+              [+] {}
+              [+] {}
+              [+] {}\n\n
+
+    """.format(name, start_date, end_date,day,job_title, descr, loc, start_time, finish_time, hourly_rate)
+
     user.add_job_details(job_title, descr, loc, start_time, finish_time, hourly_rate)
     print '[+] Successfully added details to user model'
+    print '[+] Running a series of 7 tests...'
 
-    print '\nChecking to see whether job can be retreived by title.'
-    print '[+] Retreiving date by date...'
-    sleep(0.05)
-    rec_obj = user.get_by_job_title(job_title)[0]
-    print rec_obj.job_title
-    print job_title.title()
+    values = ["start_date ",  "end_date",  "start_date", "day", "loc",
+             "start_time",  "finish_time", "hourly_rate", "total_hours", "month" ]
 
-    print 'IS SUCCESSFUL : ' + str(rec_obj.job_title).strip() == job_title.title()
-    count += 1
+    methods = []
+    # ??? NOT WORKING PROPERLY BECAUSE NOT ALL METHODS FOR THE RECORDS ARE PASSED
+    # ADD CODE THAT CONTAINS A LIST OF METHODS FOR THE RECORDS THAT PASSES THE ACTUAL METHODS TO THE FUNCTION
+    # THIS WILL PROPERLY BE A FOR LOOP HERE
+    # HOW TO DO THIS ????
 
-    # ADD TEST TO RETREIVE MONTH
+    tests = 0
+    for number, job_attribute in  enumerate(values):
+        print '[*] Running test number ({}) retreiving value by : ({}) '.format(number+1, job_attribute.upper())
+        sleep(1)
+        score = is_data_correct(user.get_by_job_title(job_attribute))
+        if score == len(values):
+            print '[+] Successfully passed all test attributes\n'
+            tests += 1
+        else:
+            print '[-] One or more values of the test did not pass\n'
 
-    # ADD TEST TO RETREIVE BY day
 
-    # ADD TEST TO RETREIVE hours
-
-    # ADD TEST TO RETREIVE MONTH
-
-    # ADD TEST TO RETREIVE
-
-    # ADD TEST TO RETREIVE
-
-    # ADD TEST TO RETREIVE  
+    if tests == len(values):
+        print 'SUCCESSFUL PASSED ALL TESTS : '
+    else:
+        print '[-] FAILED TESTS -> NUMBER OF TEST FAILED ; {} '.format(tests)
