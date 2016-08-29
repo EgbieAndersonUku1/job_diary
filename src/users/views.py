b7ba123c903dd9348c7205bae6c4da828ce6c031
jayunderwood2011 <jayunderwood2011@hotmail.com>
2016-08-29 06:35:32 +0100

    added new functions and created the admin registration interfaces

--- src/users/views.py
+++ src/users/views.py
@@ -3,7 +3,6 @@ from job_diary import app
 from flask import render_template
 from user_form_helpers import login_helper, register_helper
 
-
 # use the _login_helper to log the user in
 @app.route('/login', methods=('GET', 'POST'))
 def login():
@@ -14,7 +13,8 @@ def login():
     return login_helper(LoginForm, error, 'username', 'success', url)
 
 # use the login helper to help assist the user in logging into admin console
-@app.route('/admin', methods=('GET', 'POST'))
+@app.route('/admin/', methods=('GET', 'POST'))
+@app.route('/admin/login', methods=('GET', 'POST'))
 def admin():
     """Allows the user entry as admin"""
 
@@ -24,7 +24,7 @@ def admin():
 
 @app.route('/admin/register', methods=('GET', 'POST'))
 def admin_register():
-    return register_helper(AdminForm, 'Incorrect admin name', 'user/admin_login.html', 'success')
+    return register_helper(AdminForm, 'Incorrect admin name', 'user/admin.html', 'success')
 
 @app.route('/register', methods=('GET', 'POST'))
 def user_register():
